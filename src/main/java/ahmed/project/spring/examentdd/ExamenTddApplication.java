396e97509213d5a3c850c0305e1190f5e0c266f1
ahmed <ahmed.dahdouh98@gmail.com>
2025-04-17 09:12:28 +0200

    code de base application

---
+++ src/main/java/ahmed/project/spring/examentdd/ExamenTddApplication.java
@@ -0,0 +1,13 @@
+package ahmed.project.spring.examentdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ahmed/project/spring/examentdd/ExamenTddApplicationTests.java
@@ -0,0 +1,13 @@
+package ahmed.project.spring.examentdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
